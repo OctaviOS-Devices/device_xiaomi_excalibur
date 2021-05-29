406b755a4aef5ae0e4dd10af62e53e422a3427d7
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-05-29 11:30:14 +0530

    excalibur: add hals to clone from vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
 # Hals
 rm -rf hardware/qcom/audio/adsprpcd
+
 rm -rf hardware/qcom-caf/sm8150/audio
+git clone https://github.com/MrJarvis698/android_hardware_qcom_audio.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
+
 rm -rf hardware/qcom-caf/sm8150/display
+git clone https://github.com/MrJarvis698/android_hardware_qcom_display.git -b 11-caf-sm8150-octavi hardware/qcom-caf/sm8150/display
+
 rm -rf hardware/qcom-caf/sm8150/media
-
-# lineage hardware
-rm -rf hardware/xiaomi
-
-# Proton-Clang
-rm -rf prebuilts/clang/host/linux-x86/clang-12
+git clone https://github.com/MrJarvis698/android_hardware_qcom_media.git -b arrow-11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
